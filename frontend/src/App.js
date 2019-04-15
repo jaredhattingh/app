f06206ee933618503d771ff5c3decc06687a8753
Jared Hattingh <hattingh.jared@gmail.com>
2019-04-15 07:25:48 -0500

    again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import Navbar from './Components/Navbar/Navbar'
-import Navbar2 from './Components/Navbar/Navbar2'
-import Inventory from './Pages/Inventory'
-
-
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
-        <Navbar2/>
-        <Inventory/>
+        <Button type="primary">Button</Button>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
